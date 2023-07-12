dbe3b91c21c3ba80c25053e06e3d7265c0a0d25e
shaq <sharkpride777@gmail.com>
2023-07-12 16:49:46 +0100

    backup

--- src/Nav.js
+++ src/Nav.js
@@ -1,23 +1,21 @@
 import React from 'react';
+import { Link } from "react-scroll";
 
-                                                                
 function Nav() {
   return (
     <>
-      <div className="navcontainer">
-        <Link to='https://Sofrosyn.com' className="nav-logo">
-          <img src= './Images/sofrosyn.svg' alt="img" />
-        </Link>
-        <div className="nav-content">
-          <Link to='/home'>Home</Link>
-          <Link   to="/ourwork">Our work</Link>
-          <Link   to="/how">How it works</Link>
-          <Link   to="/benefit">Benefits</Link>
-          <Link   to="/services">Services</Link>
-          <Link   to="/pricing">Pricing</Link>
-        </div>
-      </div>    
-
+        <div className="navcontainer">
+            <Link to='https://Sofrosyn.com' className="nav-logo">
+                <img src= './Images/sofrosyn.svg' alt="img" />
+             </Link>
+            <div className="nav-content">
+                <Link   to="/ourwork">Our work</Link>
+                <Link   to="/how">How it works</Link>
+                <Link   to="/benefit">Benefits</Link>
+                <Link   to="/services">Services</Link>
+                <Link   to="/pricing">Pricing</Link>
+            </div>
+        </div>    
     </>
   )
 }
